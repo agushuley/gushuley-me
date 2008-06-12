d8810f1dde2a9cac288b46bf1ad8163bf9fb3ab5
Andriy.Gushulay <Andriy.Gushulay@b938398c-a44b-0410-b363-bbd02ca29aac>
2008-06-12 06:51:07 +0000

    Date parse routines.

---
+++ trunk/src/com/gushuley/me/date/InvalidDateException.java
@@ -0,0 +1,11 @@
+package com.gushuley.me.date;
+
+public class InvalidDateException extends Exception {
+	public InvalidDateException() {
+		super();
+	}
+
+	public InvalidDateException(String s) {
+		super(s);
+	}
+}
